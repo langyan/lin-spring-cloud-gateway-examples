14623f6b45b3c564024d117bf765d575df54ccc2
langyan <langyan.lin@gmail.com>
2018-06-30 00:01:18 +0800

    初始化工程

---
+++ simple-host-route/src/main/java/com/lin/springcloud/gateway/simple/hostroute/GatewayApplication.java
@@ -0,0 +1,12 @@
+package com.lin.springcloud.gateway.simple.hostroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
---
+++ simple-service-provider/src/main/java/com/lin/springcloud/gateway/simple/service/ServiceApplication.java
@@ -0,0 +1,11 @@
+package com.lin.springcloud.gateway.simple.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+}
